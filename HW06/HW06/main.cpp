a2c5e3e75b4b5e59955103c0cbe4efe17a160270
Shayna Snyder <ssnyder19@my.whitworth.edu>
2016-11-08 16:15:14 -0800

    Finished 13.2

--- HW06/HW06/main.cpp
+++ HW06/HW06/main.cpp
@@ -14,31 +14,51 @@ int main()
 {
     //13.1 Create a text file
     //fstream object
-    fstream inout;
+    fstream inout1;
     //open a file
-    inout.open("Exercise12_1.txt");
+    inout1.open("Exercise13_1.txt");
     //if statements determine if the file already exists
-    if(inout.fail())
+    if(inout1.fail())
     {
         //creates a file if it does not already exist
-        inout.open("Exercise12_1.txt", ios::out);
+        inout1.open("Exercise13_1.txt", ios::out);
     }
     else
     {
         //appends to the file if it already exists
-        inout.open("Exercise12_1.txt", ios::out | ios::app);
+        inout1.open("Exercise13_1.txt", ios::out | ios::app);
     }
     //writes 100 random integers into file
     for(int i=0; i<100; i++)
     {
-        inout << rand()%1000 << setw(1);
+        inout1 << rand()%1000 << " ";
     }
     //closes stream
-    inout.close();
+    inout1.close();
     
     
     //13.2 Count Characters
     //Write a program that prompts the user to enter a file name and displays the number of characters in the file.
+    //declaring string variable for the file name
+    string filename;
+    //entering user input into file name
+    cout << "Enter a file name: ";
+    cin >> filename;
+    //if statements determine if the file exists
+    fstream inout2(filename.c_str());
+    if(inout2.fail())
+        cout << filename << " does not exist." << endl;
+    else
+    {
+        //if the file exists the number of characters in the file is displayed
+        char s[1000]; //declares an array of 1000 bytes
+        inout2.read(s,1000);
+        cout << "Number of characters in " << filename << ": " << inout2.gcount() << endl;
+        //appends a c-string terminator
+        s[inout2.gcount()]='\0';
+        //closes file
+        inout2.close();
+    }
     
     
     //13.5 Baby name popularity ranking
